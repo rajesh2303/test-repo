77c7f978aa59a30f00d33c1f1766e2a5e82e8ed2
Ajith Kumar <56600154+ajithkumarg23@users.noreply.github.com>
2023-12-11 15:57:10 +0530

    Mobile menu + font change

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
-    typography: {
-        fontFamily: 'The Seasons Bold',
-    },
+    // typography: {
+    //     fontFamily: 'Roboto',
+    // },
 })
